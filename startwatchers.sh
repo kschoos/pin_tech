d1493668e1fa43dbee35a6faf26c46238a36cad6
Kai Schoos <registrierung@skusku.org>
2016-04-29 20:39:21 +0200

    Changed gitignore

--- startwatchers.sh
+++ startwatchers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gnome-terminal -e "babel --presets es2016,react -w src/ -d ."
-gnome-terminal -e "sass --watch sass:public/css"
+mate-terminal -e "babel --presets es2016,react -w src/ -d ."
+mate-terminal -e "sass --watch sass:public/css"
