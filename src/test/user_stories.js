7587c447565ad91e764d3dbcfec97cdc7441a5f0
Kai Schoos <registrierung@skusku.org>
2016-05-03 10:35:59 +0200

    Fixed .gitignore Issues

---
+++ src/test/user_stories.js
@@ -0,0 +1,23 @@
+var expect = require("chai").expect;
+
+// User Stories:
+//
+// - As an unauthenticated user: 
+// I can login with twitter
+// I can browse other users' walls of images
+// -------------------------------------------------------------------------------
+//
+// - As an authenticated user:
+// I can link to images
+// I can delete images that I've linked to
+// I can see a Pinterest-style wall of all the images I've linked to
+// If I upload an image that is broken, it will be replaced by a placeholder image
+// --------------------------------------------------------------------------------
+
+describe("User Stories", ()=>{
+  describe("As Unauthenticated User", ()=>{
+  })
+
+  describe("As Authenticated User", ()=>{
+  })
+})
