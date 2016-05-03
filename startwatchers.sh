404403baff9e2c475e554c667c586742f758e3c7
Kai Schoos <registrierung@skusku.org>
2016-05-03 12:46:50 +0200

    Removed startwatchers and relocated watch scripts to package.json. AWESOME"

--- startwatchers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-x-terminal-emulator -e "babel --presets es2016,react -w src/ -d ."
-x-terminal-emulator -e "sass --watch sass:public/css"
-x-terminal-emulator -e "watchify public/js/main.js -o public/js/bundle.js -v"
